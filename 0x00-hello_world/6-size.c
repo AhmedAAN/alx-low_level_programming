10ca60960e34009c7cff163f04c45cb4486ffe4f
AhmedAAN <you@example.com>
2023-02-16 13:13:53 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main - Entery point
+/**
+ * main - Entry point
  *
- * Return 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void){
 	char a;
@@ -16,4 +16,6 @@ int main(void){
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
 }
+
