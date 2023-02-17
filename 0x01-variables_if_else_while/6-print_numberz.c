4c5d2b691fe926af0b421a27de1ec747af1cb446
AhmedAAN <you@example.com>
2023-02-17 09:37:03 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - Entery point
+ *
+ * Description: 'fsfs'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	
+	return (0);
+}
