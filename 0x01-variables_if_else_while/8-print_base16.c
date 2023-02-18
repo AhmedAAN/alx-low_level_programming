673caec34c25ce534514f0012715df1c1e7c822b
AhmedAAN <you@example.com>
2023-02-17 16:56:45 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; n <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
