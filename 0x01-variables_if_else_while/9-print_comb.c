983b5cd81b7188916ee48a86c053d0440fdfa928
AhmedAAN <you@example.com>
2023-02-17 17:09:31 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int com = 48;
+	int space = 32;
+
+	for (n = 48; n <= 56; n++)
+	{
+		putchar(n);
+		putchar(com);
+		putchar(space);
+	}
+	putchar(57);
+
+	return (0);
+}
