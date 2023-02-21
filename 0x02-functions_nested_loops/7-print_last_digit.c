696b7907fcf7cf8449f2e8a7bdae44a89d80207d
AhmedAAN <you@example.com>
2023-02-21 15:09:31 -0800

    ssds

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+vi 7-print_last_digit.c
+
+#include "main.h"
+/**
+ * print_last_digit-prints the last digit of an interger.
+ * @a:number to compute last digit.
+ * Return:last digit.
+ */
+int print_last_digit(int x)
+{
+	int n;
+
+	n = x % 10;
+	if (n < 0)
+	{
+		n = -n;
+	}
+	_putchar(n);
+
+	return (n);
+}
