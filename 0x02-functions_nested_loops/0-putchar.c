5706e08bb4d523334de47a0260369f5b3e7afe47
AhmedAAN <you@example.com>
2023-02-21 11:52:22 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdioh>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m[] = {"_", "p", "u", "t", "c", "h", "a", "r"};
+	int n;
+
+	for (n = 0; n <= 7; n++)
+	{
+		putchar(m[n]);
+	}
+	putchar('\n');
+
+	return (0);
+}
