2f20db59de77af709152865f883b25ac9ed00689
AhmedAAN <you@example.com>
2023-02-23 13:03:23 -0800

    sdsdsd

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - sddsds
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = n; i < 0; i--)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
