6bb4a5ca7e6624ef798fc67423cdbfe4ec66eb6c
AhmedAAN <you@example.com>
2023-03-02 14:58:49 -0800

    sdfs

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,23 @@
-#include "main.h"
+include "main.h"
 
 /**
- * _strcmp - dsgsdgsdfsdg
- * @s1: sdfsdfsdfd
- * @s2: sfsdgsdgsd
- * return: sfsdfsdfsd
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	while (s1[i] == s2[i])
+	while (*s1 == *s2)
 	{
-		if (s1[i] == '\0')
+		if (*s1 == '\0')
 		{
 			return (0);
 		}
-		i++;
+		s1++;
+		s2++;
 	}
-
-	return (s1[i] - s2[i]);
+	return (*s1 - *s2);
 }
