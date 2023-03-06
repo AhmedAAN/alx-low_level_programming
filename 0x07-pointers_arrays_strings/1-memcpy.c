72636babfb71ed2a6c9d83b328029a4e0559d8fa
AhmedAAN <you@example.com>
2023-03-06 13:10:12 -0800

    sdfsdfsdfsd

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memcpy - function fill the first bytes of the memory area pointed
+ * to by  with the constant byt
+ *
+ * @dest: bytes of the memory area pointed to by @s
+ *
+ * @src: with the constant byte @b
+ *
+ * @n: memory area pointer
+ *
+ * Return: a pointer to the memory area @s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++
+	}
+
+	return (dest);
+}
