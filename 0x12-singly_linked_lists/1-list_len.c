1968ae1db96d77a0186cab8808b97c72c21b64ee
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-03-30 19:52:33 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - calcularte the number of nodes
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
