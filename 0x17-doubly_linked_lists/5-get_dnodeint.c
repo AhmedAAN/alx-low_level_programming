17974438dd2a49adf3c2399023f75fd1ebf6d09e
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-08-17 08:45:48 -0700

    create 5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returs the nth child of a list.
+ * @head: a pointer to the list
+ * @index: the index of the node
+ * Return: the address of the nth element, or NULL if it failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter;
+
+	if (head != NULL)
+	{
+		while (head->prev != NUll)
+			head = head->prev;
+	}
+
+	counter = 0;
+	while (head->next != NULL)
+	{
+		if (counter == index)
+			return (head);
+		head = head->next;
+		counter++;
+	}
+
+	return (NULL)
+}
