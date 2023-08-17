c03e0bc79c3d3a5bbd70d9a41e2d35269010440c
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-08-17 05:34:31 -0700

    update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,21 +5,21 @@
  * @h: a pointer to the list
  * Return: the number of nodes
  */
-size_t dlistint_len(const dlistint_t *h){
-  int num;
-  
-  num = 0;
-  if (h == NULL){
-    return (num);
-  }
-  while (h->prev != NULL){
-    h = h->prev;
-  }
-  
-  while (h != NULL){
-    h = h->next;
-    num++;
-  }
-  
-  return (num);
+size_t dlistint_len(const dlistint_t *h)
+{
+	int num;
+
+	num = 0;
+	if (h == NULL)
+		return (num);
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num++;
+	}
+
+	return (num);
 }
