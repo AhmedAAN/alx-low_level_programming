a76bd39322366965b1b7fddd50c5a88231840955
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-08-17 17:55:44 +0300

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->n = n;
+	new->prev = NULL;
 
 	current = *head;
 	if (current != NULL)
