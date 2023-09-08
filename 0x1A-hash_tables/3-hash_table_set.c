e1d243a1dd7a48c8aeb41555f91c772759d12094
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-09-09 01:30:23 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, i;
 	char *value_copy;
-	hash_node_t new;
+	hash_node_t *new;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	{
